1aed35189b0b00b87d16953905bbbcabd1b83962
root <root@Pablo.localdomain>
2020-11-21 21:46:53 +0100

    Sounds and styles added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+ 
+  const clickSound = document.getElementById('clickAudio')
 
+  document.getElementById('log').onclick = () => {
+    const logRedirect = ()=>{
+      setTimeout(() => {
+        
+      }, 0);
+    }
+    clickSound.play()
+  }
   
+  document.getElementById('sign').onclick = () => {
+    clickSound.play()
+    console.log('signclick')
+  }
+  console.log(document.getElementById('audio'))
+
 
 }, false);
 
 
-// window.onload = ()=>{
-//   const sound = document.getElementById("background-audio")
-
-//   const soundOn = ()=>{
-//     sound.volume = .1
-//     sound.autoplay = true
-//   }
-// }
