b412a4b87be37047bb4db5f6a45f544d7104747c
root <root@Pablo.localdomain>
2020-11-23 15:06:55 +0100

    Styles to Pokemons added

--- public/javascripts/pokemonScript.js
+++ public/javascripts/pokemonScript.js
@@ -9,21 +9,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
   axios.get(`https://pokeapi.co/api/v2${pokemonPath}`)
   .then((result)=>{
+    const name = result.data.name.toUpperCase()
+    const typeLetter = result.data.types[0].type.name.slice(0,1).toUpperCase()
+    const upperCasedType = typeLetter + result.data.types[0].type.name.slice(1)
+    const abilityLetter = result.data.abilities[0].ability.name.slice(0,1).toUpperCase()
+    const upperCasedAbility = abilityLetter + result.data.abilities[0].ability.name.slice(1)
       pokedisplay.innerHTML = `
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${result.data.id}.png" style="width: 150px;">
-      <p>Name: ${result.data.name}</p>
-      <p>Weight: ${result.data.weight}</p>
+      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${result.data.id}.png" style="width: 300px;">
+      <p class="name-title">Nº${result.data.id}. ${name}</p>
+      <p><span>Weight:</span> ${result.data.weight} lb</p>
+      <p><span>Height:</span> ${result.data.height} in</p>
+      <p><span>Type:</span> ${upperCasedType}</p>
+      <p><span>Ability:</span> ${upperCasedAbility}</p>
+      
   
       <form action="/addteam/${result.data.name}/${result.data.id}" method="POST">
-        <button type="submit">Add to my team</button>
+        <button class="btn-pkmn" type="submit">Add to my team</button>
       </form>
   
       <form action="/capture/${result.data.name}/${result.data.id}" method="POST">
-        <button id="capture-button" type="submit">Capture</button>
+        <button class="btn-pkmn" id="capture-button" type="submit">Capture</button>
       </form>
   
       <form action="/remove/${result.data.name}" method="POST">
-        <button type="submit">Remove from team</button>
+        <button class="btn-pkmn" type="submit">Remove from team</button>
       </form>
       `;
   })
