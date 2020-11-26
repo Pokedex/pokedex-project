afed45017d85d291cd37636f1115f32d46b4d813
Sergio <srosduran@gmail.com>
2020-11-26 16:23:46 +0100

    loading page function implemented correctly

--- public/javascripts/pokedexScript.js
+++ public/javascripts/pokedexScript.js
@@ -5,18 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const pikachuload = document.getElementById('pikachu-load');
   const loadbox = document.getElementById('load-box');
   const capturedArr = document.getElementById('data-transport').innerText.split('-');
+  let counter = 0;
 
-  setTimeout(()=>{
-    pikachuload.style.display = 'none';
-    loadbox.style.display = 'block';
-  }, 6500);
-
+    setInterval(()=>{
+      [...pokedexdisplay.children].forEach((item)=>{
+           if(item.childNodes[0].complete){
+            counter++;
+          }
+          if(counter===151){
+            pikachuload.style.display = 'none';
+            loadbox.style.display = 'block';
+          }  
+        });
+    },100);
+      
   axios.get(`https://pokeapi.co/api/v2/pokemon?offset=0&limit=151`)
   .then((result)=>{
     result.data.results.forEach((pokemon, index)=>{
       const number = index + 1;
       const pokeImg = document.createElement('img');
       const pokeAnchor = document.createElement('a');
+
+
       if(capturedArr.includes(pokemon.name)){
         pokeImg.setAttribute('style', 'opacity: 1;');
       } else {
@@ -26,20 +36,14 @@ document.addEventListener('DOMContentLoaded', () => {
       pokeAnchor.setAttribute('href', `/pokemon/${number}`);
       pokeImg.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${number}.png`);
       pokeImg.setAttribute('alt', `${pokemon.name} image`);
-      // pokeImg.setAttribute('id', `${pokemon.name}-pokedex`)
       pokeAnchor.append(pokeImg);
       pokedexdisplay.append(pokeAnchor);
-
+      
     });
     
   })
   .catch((err)=>{
     console.log(err);
   });
-
-
-  
-
-
 
 }, false);
